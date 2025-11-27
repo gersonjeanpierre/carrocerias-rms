3ffb5aa1f7bf2718d8b72e97f5ff0548ccf92884
Gerson Jean Pierre <jalukone@outlook.com>
2025-11-27 18:08:51 -0500

    Change footer

--- src/app/features/footer/footer.ts
+++ src/app/features/footer/footer.ts
@@ -2,20 +2,76 @@ import { Component } from '@angular/core';
 
 interface FooterLink {
   label: string;
-  href: string;
+  href?: string;
+  icon: string; // clase bi o span
+  isSpan?: boolean; // si es span en vez de icono
 }
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.html',
-  styleUrls: ['./footer.css'],
+  templateUrl: './footer.html'
 })
 export class FooterComponent {
-  links: FooterLink[] = [
-    { label: 'Inicio', href: '#' },
-    { label: 'Productos', href: '#productos' },
-    { label: 'Servicios', href: '#servicios' },
-    { label: 'Contáctanos', href: '#contacto' },
+  menuLinks: FooterLink[] = [
+    {
+      label: 'Inicio',
+      href: '#',
+      icon: 'icon-[material-symbols--arrow-right-rounded]'
+    },
+    {
+      label: 'Productos',
+      href: '/productos',
+      icon: 'icon-[material-symbols--arrow-right-rounded]'
+    },
+    { label: 'Descúbrenos', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' },
+    { label: 'Servicios', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' },
+    { label: 'Blog', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' },
+    { label: 'Contáctanos', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' }
+  ];
+
+  productLinks: FooterLink[] = [
+    { label: 'Brazos de Izaje', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' },
+    { label: 'Remolques', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' },
+    {
+      label: 'Tanques Cisterna de Vacío',
+      href: '#',
+      icon: 'icon-[material-symbols--arrow-right-rounded]'
+    },
+    {
+      label: 'Unidades Especiales',
+      href: '#',
+      icon: 'icon-[material-symbols--arrow-right-rounded]'
+    },
+    { label: 'Contenedores', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' },
+    { label: 'Ver Más', href: '#', icon: 'icon-[material-symbols--arrow-right-rounded]' }
+  ];
+
+  contactLinks: FooterLink[] = [
+    {
+      label: '962 304 464',
+      href: 'tel:+51962304464',
+      icon: 'icon-[material-symbols--phone-enabled-sharp]'
+    },
+    {
+      label: '993 056 995',
+      href: 'tel:+51993056995',
+      icon: 'icon-[material-symbols--phone-enabled-sharp]'
+    },
+    {
+      label: 'Asc. Huerta Granja en Ayllu, Lima',
+      icon: 'icon-[material-symbols--location-on]',
+      isSpan: true
+    },
+    {
+      label: 'ventas@rmscarrocerias.com.pe',
+      href: 'mailto:ventas@rmscarrocerias.com.pe',
+      icon: 'icon-[material-symbols--mail]'
+    },
+    {
+      label: 'ventas01@rmscarrocerias.com.pe',
+      href: 'mailto:ventas01@rmscarrocerias.com.pe',
+      icon: 'icon-[material-symbols--mail]'
+    }
   ];
 
   currentYear = new Date().getFullYear();
