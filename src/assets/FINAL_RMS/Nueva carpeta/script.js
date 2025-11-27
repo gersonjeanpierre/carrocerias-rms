b8edaaaf9a65ee559aa3f0b8d9d6beb12a2ba435
Gerson Jean Pierre <jalukone@outlook.com>
2025-11-27 12:01:18 -0500

    design final in vanilla

--- src/assets/FINAL_RMS/Nueva carpeta/script.js
+++ src/assets/FINAL_RMS/Nueva carpeta/script.js
@@ -1,9 +1,10 @@
+
 var swiper = new Swiper(".proyectosSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
     autoplay: {
-        delay: 3000,
+        delay: 2000,
         disableOnInteraction: false,
     },
     pagination: {
@@ -21,12 +22,12 @@ var swiper = new Swiper(".proyectosSwiper", {
     }
 });
     var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,       
+    slidesPerView: 5,        // cuántos logos visibles
     spaceBetween: 30,
     loop: true,
     centeredSlides: false,
     autoplay: {
-        delay: 2000,           
+        delay: 2000,           // 3 segundos
         disableOnInteraction: false,
     },
     breakpoints: {
@@ -37,12 +38,30 @@ var swiper = new Swiper(".proyectosSwiper", {
     }
     });
     var swiper = new Swiper(".productosSwiper", {
-    slidesPerView: 4,         
+    slidesPerView: 4,          // 4 columnas como tu ejemplo
     spaceBetween: 0,
     loop: true,
     autoplay: {
-        delay: 3000,           
+        delay: 2000,           // Cambia cada 2.5 segundos
         disableOnInteraction: false,
     },
-    speed: 800,                 
+    speed: 800,                 // transición suave
+});
+const carousel = document.getElementById('heroCarousel');
+
+carousel.addEventListener('slide.bs.carousel', function (event) {
+  const nextSlide = event.relatedTarget;
+  const bgImage = nextSlide.getAttribute('data-bg');
+
+  nextSlide.style.setProperty('--bg-image', `url(${bgImage})`);
+});
+
+// Inicializar fondo del primer slide
+document.querySelectorAll('.carousel-item').forEach(item => {
+  const bg = item.getAttribute('data-bg');
+  item.style.setProperty('--bg-image', `url(${bg})`);
+});
+document.querySelectorAll('.carousel-item').forEach(item => {
+  const bg = item.getAttribute('data-bg');
+  item.style.setProperty('--bg-image', `url('${bg}')`);
 });
