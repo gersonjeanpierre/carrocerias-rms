adf06c6d3f152e1a5e42703853a55698e5e25fc1
Gerson Jean Pierre <jalukone@outlook.com>
2025-11-27 12:19:27 -0500

    rename, move and config

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,18 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './features/header/header';
 import { Model3d } from './features/model3d/model3d';
-import { SliderComponent } from './features/slider/slider';
+import { SliderComponent } from './features/landing/slider/slider';
 import { HeroComponent } from './features/hero/hero';
 import { CompanyInfoComponent } from './features/company-info/company-info';
 import { ProductsComponent } from './features/products/products';
-import { CatalogCtaComponent } from './features/catalog-cta/catalog-cta';
+import { CatalogCtaComponent } from './features/landing/catalog-cta/catalog-cta';
 import { ProjectsCarouselComponent } from './features/projects-carousel/projects-carousel';
-import { BrandsCarouselComponent } from './features/brands-carousel/brands-carousel';
-import { VideoComponent } from './features/video/video';
+import { BrandsCarouselComponent } from './features/landing/brands-carousel/brands-carousel';
+import { VideoComponent } from './features/landing/video/video';
 import { FooterComponent } from './features/footer/footer';
-import { EquiposMedidaComponent } from './features/equipos-medida/equipos-medida';
-import { ProductosVendidosComponent } from './features/productos-vendidos/productos-vendidos';
-import { ProyectosComponent } from './features/proyectos/proyectos';
+import { EquiposMedidaComponent } from './features/landing/equipos-medida/equipos-medida';
+import { ProductosVendidosComponent } from './features/landing/productos-vendidos/productos-vendidos';
+import { ProyectosComponent } from './features/landing/proyectos/proyectos';
 
 @Component({
   selector: 'app-root',
@@ -27,9 +27,9 @@ import { ProyectosComponent } from './features/proyectos/proyectos';
     VideoComponent,
     BrandsCarouselComponent,
     Model3d,
-    FooterComponent,
+    FooterComponent
   ],
-  templateUrl: './app.html',
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('carrocerias-rms');
---
+++ src/app/features/products/product-list/product-list.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.html'
+})
+export class ProductList {}
