e1d48d25f6fd13098602b63168a1aafe0a7f0faf
Gerson Jean Pierre <jalukone@outlook.com>
2025-12-05 13:20:16 -0500

    font in opfs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { FooterComponent } from './layout/footer/footer';
 import { SliderComponent } from './layout/slider/slider';
+import { FontLoaderService } from './core/services/font-loader-service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,10 @@ import { SliderComponent } from './layout/slider/slider';
 })
 export class App {
   protected readonly title = signal('Carrocerias RMS');
+  private readonly fontLoaderService = inject(FontLoaderService);
+
+  constructor() {
+    // Inicializar la carga de fuentes
+    this.fontLoaderService.initFontLoader();
+  }
 }
